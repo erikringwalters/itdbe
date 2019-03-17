e00d325674e303e73cb59ea9853404e6b770a491
erikringwalters <erik.ring.walters@gmail.com>
2019-03-17 23:43:14 +0000

    running into server errors

--- app/main.py
+++ app/main.py
@@ -1,25 +1,10 @@
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
         return "Hello World from Flask"
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
-
-
